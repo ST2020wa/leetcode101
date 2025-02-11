de0ecac818bb0f16511740f8495cf38d1fb9051f
“ST <shaotian.li.april@gmail.com>
2025-02-11 16:46:48 +0800

    add candy

--- leetcode101.ts
+++ leetcode101.ts
@@ -13,3 +13,22 @@ let cookieKid = (cookies, kids) => {
     }
     return kidsIndex;
   }; 
+
+
+//https://leetcode.com/problems/candy/description/
+let candy = function(ratings) {
+  const candies = new Array(ratings.length).fill(1);
+  // from index=1 to index = length-1
+  for(let i = 1; i<=ratings.length-1; i++){
+      if(ratings[i]>ratings[i-1]){
+          candies[i]=candies[i-1]+1
+      }
+  };
+  // from index=length-2 to index = 0, note that we need to compare the max value between the current value and the value from the right side
+  for(let j = ratings.length-2; j>=0; j--){
+      if(ratings[j]>ratings[j+1]){
+          candies[j]=Math.max(candies[j], candies[j+1]+1)
+      }
+  }
+  return candies.reduce((a, c) => a + c, 0);
+};
