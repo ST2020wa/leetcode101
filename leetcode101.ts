a7d2518a112295eb30bd8c287e37727df43a388b
“ST <shaotian.li.april@gmail.com>
2025-02-17 23:01:07 +0800

    167

--- leetcode101.ts
+++ leetcode101.ts
@@ -148,3 +148,23 @@ let maxProfit = function(prices) {
     }
     return maxOutput;
 };
+
+/* Chapter 2_Two Pointers */
+
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// when iteration time is not fixed, we use two pointers with while loop; when iteration time is fixed, we use for loop
+let twoSum = function(numbers, target) {
+    let i = 0;
+    let j = numbers.length-1;
+    while(i<j){
+        let sum = numbers[i]+numbers[j]
+        if(sum === target){
+            break;
+        }else if(sum > target){
+            j--;
+        }else{
+            i++;
+        }
+    }
+    return [i+1, j+1];
+};
