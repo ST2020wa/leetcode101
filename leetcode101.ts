deb822b6cf8444a57d53f258fca4938f093b5dce
“ST <shaotian.li.april@gmail.com>
2025-03-11 12:27:51 +0800

    130

--- leetcode101.ts
+++ leetcode101.ts
@@ -843,3 +843,29 @@ let shortestBridge = function(grid) {
    return level;
 };
 }
+
+//https://leetcode.com/problems/surrounded-regions/
+let surroundedRegions = ()=>{
+    let solve=(board)=>{
+        if(!board || board.length === 0)return;
+        let m = board.length,n=board[0].length;
+    
+        let dfs=(i,j)=>{
+            if(i<0||i>=m||j<0||j>=n||board[i][j]!=='O')return;
+            board[i][j]="T"
+            dfs(i+1,j);dfs(i-1,j);dfs(i,j+1);dfs(i,j-1);
+        }
+        for(let i=0;i<m;i++){
+            dfs(i,0);dfs(i,n-1); // first and last row
+        }
+        for(let j=0;j<n;j++){
+            dfs(0,j);dfs(m-1,j); // first and last column
+            }
+            for(let i=0;i<m;i++){
+                for(let j=0;j<n;j++){
+                    if(board[i][j]==='O')board[i][j]='X';
+                    if(board[i][j]==='T')board[i][j]='O';
+                }
+            }
+    }
+}
